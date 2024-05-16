0cc358f7391a92308a1a6d3d37534b6355df5692
sleeprince <wowkwangsu@gmail.com>
2024-05-16 16:44:54 +0900

    first lesson

---
+++ src/Test.java
@@ -0,0 +1,26 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int num = 1;
+		double num2 = 3/2f;
+		//System.out.println(num2 + "");
+		
+		int no = 10;
+		no = no + 2;
+		System.out.println(no + "");
+		no += 2;
+		System.out.println(no + "");
+		no -= 2;
+		System.out.println(no + "");
+		no *= 2;
+		System.out.println(no + "");
+		no /= 2;
+		System.out.println(no + "");
+		no %= 2;
+		System.out.println(no + "");
+		
+	}
+
+}
